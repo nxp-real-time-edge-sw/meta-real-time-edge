532ebb02968291c7558eb4078d0ced7dce9aca82
Pramod Kumar <pramod.kumar_1@nxp.com>
2024-03-08 08:11:18 +0530

    rfnm: add python pip3 support
    Signed-off-by: Pramod Kumar <pramod.kumar_1@nxp.com>

--- tools/real-time-edge-setup-env.sh
+++ tools/real-time-edge-setup-env.sh
@@ -68,7 +68,7 @@ change_conf()
 	echo "EXTRA_IMAGE_FEATURES += \" debug-tweaks tools-debug eclipse-debug ssh-server-openssh \"" >> $BUILD_DIR/conf/local.conf
 	echo "CORE_IMAGE_EXTRA_INSTALL += \" openssh-sftp openssh-sftp-server \"" >> $BUILD_DIR/conf/local.conf
 	echo "IMAGE_INSTALL:append = \" kernel-module-la9310 userapp-la9310 freertos-la9310 arm-ral dpdk kernel-module-kpage-ncache \"" >> $BUILD_DIR/conf/local.conf
-	echo "IMAGE_INSTALL:append = \" imx-test dtc \"" >> $BUILD_DIR/conf/local.conf
+	echo "IMAGE_INSTALL:append = \" imx-test dtc python3-pip \"" >> $BUILD_DIR/conf/local.conf
 
 	if [ "${RFNM_CLI_ENABLE}" = "enable" ]
 	then
