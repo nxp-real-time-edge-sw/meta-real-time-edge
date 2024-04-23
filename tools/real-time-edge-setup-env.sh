f278006e376b5991a48f4f90a2b58294b5358c04
Pramod Kumar <pramod.kumar_1@nxp.com>
2024-04-23 17:57:03 +0530

    rfnm: add m7 binary in rootfs
    Signed-off-by: Pramod Kumar <pramod.kumar_1@nxp.com>

--- tools/real-time-edge-setup-env.sh
+++ tools/real-time-edge-setup-env.sh
@@ -65,7 +65,7 @@ change_conf()
 	echo "EXTRA_IMAGE_FEATURES += \" package-management \"" >> $BUILD_DIR/conf/local.conf
 	echo "EXTRA_IMAGE_FEATURES += \" debug-tweaks tools-debug eclipse-debug ssh-server-openssh \"" >> $BUILD_DIR/conf/local.conf
 	echo "CORE_IMAGE_EXTRA_INSTALL += \" openssh-sftp openssh-sftp-server \"" >> $BUILD_DIR/conf/local.conf
-	echo "IMAGE_INSTALL:append = \" kernel-module-la9310 userapp-la9310 freertos-la9310 arm-ral dpdk kernel-module-kpage-ncache \"" >> $BUILD_DIR/conf/local.conf
+	echo "IMAGE_INSTALL:append = \" kernel-module-la9310 userapp-la9310 freertos-la9310 arm-ral dpdk kernel-module-kpage-ncache imx-m7-la9310 \"" >> $BUILD_DIR/conf/local.conf
 	echo "IMAGE_INSTALL:append = \" imx-test dtc python3-pip \"" >> $BUILD_DIR/conf/local.conf
 
 	if test $fsl_setup_internal; then
