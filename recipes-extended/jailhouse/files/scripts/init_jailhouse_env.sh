421e06f204ea11b0c9cf679459b3e40b83ef748f
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-06-20 17:49:51 +0800

    jailhouse: update the script for install the jailhouse driver module
    Change to use modprobe command to install jailhouse.ko.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- recipes-extended/jailhouse/files/scripts/init_jailhouse_env.sh
+++ recipes-extended/jailhouse/files/scripts/init_jailhouse_env.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ ! -f "../inmates/kernel/jailhouse.ko" ]; then
-	echo "copy jailhouse.ko from /lib/modules/`uname -r`/extra/driver/"
-	cp /lib/modules/`uname -r`/extra/driver/jailhouse.ko ../inmates/kernel/
-fi
-
-insmod ../inmates/kernel/jailhouse.ko;
+modprobe jailhouse.ko
