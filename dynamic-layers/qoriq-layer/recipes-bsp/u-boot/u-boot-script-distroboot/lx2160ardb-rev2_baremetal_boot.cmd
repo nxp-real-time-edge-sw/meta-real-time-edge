2da4326edbe829506d383f65b46747d4d7e5c4c7
Jianchao Wang <jianchao.wang_1@nxp.com>
2026-07-03 13:45:30 +0800

    baremetal: Modify the execution order of the cpu command in boot.cmd
    - ls1043ardb_baremetal_boot.cmd
    - ls1046ardb_baremetal_boot.cmd
    - lx2160ardb-rev2_baremetal_boot.cmd
    
    Signed-off-by: Jianchao Wang <jianchao.wang_1@nxp.com>

--- dynamic-layers/qoriq-layer/recipes-bsp/u-boot/u-boot-script-distroboot/lx2160ardb-rev2_baremetal_boot.cmd
+++ dynamic-layers/qoriq-layer/recipes-bsp/u-boot/u-boot-script-distroboot/lx2160ardb-rev2_baremetal_boot.cmd
@@ -10,6 +10,8 @@ env exists baremetal_image || setenv baremetal_image bm-u-boot.bin;
 part uuid $devtype $devnum:$devpart_root partuuidr;
 setenv bootargs "console=ttyAMA0,115200 earlycon=pl011,mmio32,0x21c0000 root=PARTUUID=$partuuidr rw rootwait pci=pcie_bus_perf maxcpus=1 memmap=1152M$0xb0000000 $othbootargs";
 load $devtype $devnum:$devpart_boot $baremetal_addr $baremetal_image;
+load $devtype $devnum:$devpart_boot $kernel_addr_r $kernel_image;
+load $devtype $devnum:$devpart_boot $fdt_addr_r $dtb;
 cpu 1 release ${baremetal_addr};
 cpu 2 release ${baremetal_addr};
 cpu 3 release ${baremetal_addr};
@@ -25,6 +27,4 @@ cpu 12 release ${baremetal_addr};
 cpu 13 release ${baremetal_addr};
 cpu 14 release ${baremetal_addr};
 cpu 15 release ${baremetal_addr};
-load $devtype $devnum:$devpart_boot $kernel_addr_r $kernel_image;
-load $devtype $devnum:$devpart_boot $fdt_addr_r $dtb;
 booti $kernel_addr_r - $fdt_addr_r
